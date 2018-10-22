ea0591b7c837952819832c383d175fdce5ab4c41
Rafael Römhild <rafael@roemhild.de>
2018-10-22 12:23:29 +0200

    Improve performance and memory usage
    * remove most global calls
    * remove namedtuple
    * use generator for payload
    * move HomieDevice to homie.device

--- homie/node/led.py
+++ homie/node/led.py
@@ -1,7 +1,6 @@
 from machine import Pin
 
 from homie.node import HomieNode
-from homie import Property
 
 
 ONOFF = {b'off': 0, b'on': 1, 0: b'off', 1: b'on'}
@@ -11,8 +10,14 @@ class LED(HomieNode):
 
     def __init__(self, pin=2, interval=1):
         super().__init__(interval=interval)
+        self.pin = pin
         self.led = Pin(pin, Pin.OUT, value=0)
-        self.has_new_update = True
+        self.updated = True
+
+    def __repr__(self):
+        return "LED(pin={!r}, interval={!r})".format(
+            self.pin, self.interval
+        )
 
     def __str__(self):
         return 'LED status = {}'.format(ONOFF[self.led.value()])
@@ -25,26 +30,26 @@ class LED(HomieNode):
         return [b'led']
 
     def get_properties(self):
-        yield Property(b'led/$type', b'led', True)
-        yield Property(b'led/$properties', b'power', True)
-        yield Property(b'led/power/$settable', b'true', True)
-        yield Property(b'led/power/$name', b'LED', True)
-        yield Property(b'led/power/$datatype', b'string', True)
-        yield Property(b'led/power/$format', b'on,off', True)
+        yield (b'led/$type', b'led', True)
+        yield (b'led/$properties', b'power', True)
+        yield (b'led/power/$settable', b'true', True)
+        yield (b'led/power/$name', b'LED', True)
+        yield (b'led/power/$datatype', b'string', True)
+        yield (b'led/power/$format', b'on,off', True)
 
-    def callback(self, topic, message):
-        if message == b'toggle':
-            self.led.value(not self.led.value())
+    def callback(self, topic, msg):
+        if msg == b'toggle':
+            self.led(not self.led.value())
         else:
-            self.led.value(ONOFF[message])
+            self.led(ONOFF[msg])
 
-        self.has_new_update = True
+        self.updated = True
 
     def has_update(self):
-        if self.has_new_update is True:
-            self.has_new_update = False
+        if self.updated is True:
+            self.updated = False
             return True
         return False
 
     def get_data(self):
-        yield Property(b'led/power', ONOFF[self.led.value()], True)
+        yield (b'led/power', ONOFF[self.led()], True)
